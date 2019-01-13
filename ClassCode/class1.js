3f11cce247dbaf35a5ccc66dcb84691c4e6eab54
arjunpatel <patelaj94@yahoo.com>
2019-01-13 15:12:06 -0500

    class1 code files

---
+++ ClassCode/class1.js
@@ -0,0 +1,9 @@
+
+
+var number1 = prompt("Enter a value number 1")
+var number2 = prompt("Enter a value number 2")
+number1 = parseInt(number1)
+number2 = parseInt(number2)
+console.log(typeof(number1, number2))
+answer = number1+number2
+console.log(answer)
