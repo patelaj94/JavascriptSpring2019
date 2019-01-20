4e82a132fc777c070771ea43a657c4a59b905ce4
arjunpatel <patelaj94@yahoo.com>
2019-01-20 00:45:02 -0500

    adding class 2 files

---
+++ ClassCode/class2.js
@@ -0,0 +1,22 @@
+let randomNumbers = [24, 3, 17, 31, 80]
+
+//randomNumbers.push(40);
+//console.log(randomNumbers);
+
+//randomNumbers.pop();
+//console.log(randomNumbers);
+
+//randomNumbers.shift()
+//console.log(randomNumbers)
+
+//randomNumbers.unshift(25)
+//console.log(randomNumbers)
+
+
+
+
+
+// if(5 === 5){
+//	console.log('Yes')
+//}
+// ( 5 > 10)
