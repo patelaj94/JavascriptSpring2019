50678d6e729b7df1bd3cfd4a25b2804d50c5c59f
Byron Sakiadis <byronl@udel.edu>
2019-01-27 11:44:34 -0500

    Added html front-end practice file for class 3

--- Practice/Class3Practice.js
+++ Practice/Class3Practice.js
@@ -9,4 +9,13 @@
 	// loop through this array and print each name. You can use a for or while loop
 
 // Challenge Problem 3
-	// 
+	// Yet another HTML problem!
+	// You will need google for this: I would recommend this page https://www.w3schools.com/html/html_examples.asp
+	// Take the file "index.html" under Practice/Class3Files
+	// Make the following additions to the file:
+	// 1. Add a title to the page head
+	// 2. Make a <div> inside the body which has your name
+	// 3. Make a <p> (paragraph) tag inside the body which has the name of a color you like
+	// 4. Change the <style> tag to make the <p> element the color you want from 3.
+	// 5. Inside the body tag, add a <form> tag with one text field(use this example for reference - https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_text)
+	// 6. Add a button inside the form.
